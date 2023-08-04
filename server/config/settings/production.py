62556d66d49e7abdd0df4522db22bbdea49f7b93
balzamas <d.berger@dontsniff.co.uk>
2023-08-04 09:32:42 +0200

    Change to new.waatea.vip

--- server/config/settings/production.py
+++ server/config/settings/production.py
@@ -6,7 +6,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["waatea2.waatea.vip"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["new.waatea.vip"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
