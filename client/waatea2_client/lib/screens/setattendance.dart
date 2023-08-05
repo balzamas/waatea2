dcd9caf32edffda19f935a1ebe3f6100c95d9347
balzamas <d.berger@dontsniff.co.uk>
2023-08-05 19:17:55 +0200

    Set attendendancy

--- client/waatea2_client/lib/screens/setattendance.dart
+++ client/waatea2_client/lib/screens/setattendance.dart
@@ -12,12 +12,14 @@ import '../models/availability_model.dart';
 import '../models/training_model.dart';
 import '../widgets/setavailability_row.dart';
 
+//Todo: programmiert mit Kindergeschrei im Hintergrund, total mess, aufräumen
+
 class SetAttendance extends StatefulWidget {
   late final String token;
-  late final String clubid;
-  late final int userid;
+  late final String clubId;
+  late final int userId;
   late final String season;
-  SetAttendance(this.token, this.clubid, this.userid, this.season);
+  SetAttendance(this.token, this.clubId, this.userId, this.season);
   @override
   SetAttendanceState createState() => SetAttendanceState();
 }
@@ -25,11 +27,58 @@ class SetAttendance extends StatefulWidget {
 class SetAttendanceState extends State<SetAttendance> {
   late Future<SetAttendanceModel> setAttendanceContent;
   final availabilityListKey = GlobalKey<SetAttendanceState>();
+  int state = 0;
+  String? attendanceId = "";
+  String? trainingId = null;
+  int? dayofhteyear = null;
 
   @override
   void initState() {
     super.initState();
     setAttendanceContent = getCurrentTraining();
+  }
+
+  Future setAttendanceNow(int state) async {
+    bool boolState = false;
+    if (state == 1) {
+      boolState = true;
+    }
+    if (this.attendanceId != "") {
+      final Map<String, bool> body = {
+        'attended': boolState,
+      };
+
+      final http.Response response = await http.patch(
+        Uri.parse('${globals.URL_PREFIX}/api/attendance/$attendanceId/'),
+        headers: {
+          'Authorization': 'Token ${widget.token}',
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: json.encode(body),
+      );
+    } else {
+      final Map<String, dynamic> body = {
+        'attended': boolState,
+        'club': widget.clubId,
+        'dayofyear': this.dayofhteyear,
+        'season': widget.season,
+        'player': widget.userId
+      };
+
+      final http.Response response = await http.post(
+        Uri.parse('${globals.URL_PREFIX}/api/attendance/'),
+        headers: {
+          'Authorization': 'Token ${widget.token}',
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: json.encode(body),
+      );
+
+      var data = jsonDecode(response.body);
+
+      attendanceId = data["pk"];
+    }
+    name(params) {}
   }
 
   Future<SetAttendanceModel> getCurrentTraining() async {
@@ -38,7 +87,7 @@ class SetAttendanceState extends State<SetAttendance> {
 
     final response = await http.get(
         Uri.parse(
-            "${globals.URL_PREFIX}/api/training_current/filter?club=${widget.clubid}&season=${widget.season}"),
+            "${globals.URL_PREFIX}/api/training_current/filter?club=${widget.clubId}&season=${widget.season}"),
         headers: {'Authorization': 'Token ${widget.token}'});
 
     final items = json.decode(response.body).cast<Map<String, dynamic>>();
@@ -49,10 +98,12 @@ class SetAttendanceState extends State<SetAttendance> {
     SetAttendanceModel setAttendance = SetAttendanceModel(text: "", state: 0);
 
     if (trainings.length > 0) {
+      this.trainingId = trainings[0].pk;
+      this.dayofhteyear = trainings[0].dayofyear;
       //Load attendance
       final responseAttend = await http.get(
           Uri.parse(
-              "${globals.URL_PREFIX}/api/attendances/filter?dayofyear=${trainings[0].dayofyear}&player=${widget.userid}&season=${widget.season}"),
+              "${globals.URL_PREFIX}/api/attendances/filter?dayofyear=${trainings[0].dayofyear}&player=${widget.userId}&season=${widget.season}"),
           headers: {'Authorization': 'Token ${widget.token}'});
 
       if (responseAttend.statusCode == 200) {
@@ -73,19 +124,24 @@ class SetAttendanceState extends State<SetAttendance> {
             "$time_prefix training: ${DateTime.parse(trainings[0].date).day}.${DateTime.parse(trainings[0].date).month}.${DateTime.parse(trainings[0].date).year}";
 
         if (availabilities.length == 1) {
+          attendanceId = availabilities[0].pk;
           if (availabilities[0].attended) {
             setAttendance.state = 1;
+            this.state = 1;
           } else {
             setAttendance.state = 2;
+            this.state = 2;
           }
         } else {
           setAttendance.state = 0;
+          this.state = 0;
           setAttendance.text = setAttendance.text + "\nPlease set attendancy.";
         }
       }
     } else {
       setAttendance.text = "No current training";
       setAttendance.state = -1;
+      this.state = -1;
     }
 
     return setAttendance;
@@ -116,13 +172,13 @@ class SetAttendanceState extends State<SetAttendance> {
 
               Icon icon;
 
-              if (setAttendance.state == 1) {
+              if (this.state == 1) {
                 icon = const Icon(Icons.check_circle_outline,
                     color: Colors.green, size: 150);
-              } else if (setAttendance.state == 2) {
+              } else if (this.state == 2) {
                 icon = const Icon(Icons.highlight_off_outlined,
                     color: Colors.red, size: 150);
-              } else if (setAttendance.state == 0) {
+              } else if (this.state == 0) {
                 icon = const Icon(Icons.help_outline,
                     color: Colors.orange, size: 150);
               } else {
@@ -140,31 +196,47 @@ class SetAttendanceState extends State<SetAttendance> {
                   ),
                   SizedBox(height: 10),
                   icon,
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          // Handle the "Yes" button tap
-                          // You can call a function here to set the attendance to "Yes"
-                          // For example:
-                          // setAttendanceToYes();
-                        },
-                        child: Text("Yes"),
-                      ),
-                      SizedBox(width: 20),
-                      ElevatedButton(
-                        onPressed: () {
-                          // Handle the "No" button tap
-                          // You can call a function here to set the attendance to "No"
-                          // For example:
-                          // setAttendanceToNo();
-                        },
-                        child: Text("No"),
-                      ),
-                    ],
-                  ),
+                  if (this.state > -1) ...[
+                    SizedBox(height: 20),
+                    Text(
+                      "Attending/Attended?",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            setAttendance.state = 1;
+                            this.state = 1;
+                            setState(() {
+                              this.state = 1;
+                            });
+                            setAttendanceNow(1);
+                          },
+                          child: Text("Yes",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold)),
+                        ),
+                        SizedBox(width: 20),
+                        ElevatedButton(
+                          onPressed: () {
+                            setAttendance.state = 2;
+                            this.state = 2;
+                            setState(() {
+                              this.state = 2;
+                            });
+                            setAttendanceNow(2);
+                          },
+                          child: Text("No",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold)),
+                        ),
+                      ],
+                    ),
+                  ]
                 ],
               );
             }
