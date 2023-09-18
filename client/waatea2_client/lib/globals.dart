0de79c039928c8606c36b2b72deae111f2222667
balzamas <d.berger@dontsniff.co.uk>
2023-09-18 10:57:47 +0200

    Release ready

--- client/waatea2_client/lib/globals.dart
+++ client/waatea2_client/lib/globals.dart
@@ -1,7 +1,9 @@
 import 'models/user_model.dart';
 
 //const String URL_PREFIX = "http://localhost:8000";
-const String URL_PREFIX = "https://new.waatea.vip";
+//const String URL_PREFIX = "https://new.waatea.vip";
+const String URL_PREFIX = "https://www.waatea.vip";
+
 late int playerId;
 late String seasonID;
 late String clubId;
