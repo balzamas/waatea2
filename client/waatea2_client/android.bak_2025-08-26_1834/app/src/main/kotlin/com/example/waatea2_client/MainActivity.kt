a58a2218614a5d79518b6e7b7b4c394b162da01f
ctrl <d.berger@dontsniff.co.uk>
2026-09-16 11:21:15 +0200

    Upgrade Flutter to 3.47.4

---
+++ client/waatea2_client/android.bak_2025-08-26_1834/app/src/main/kotlin/com/example/waatea2_client/MainActivity.kt
@@ -0,0 +1,6 @@
+package ch.atomikzopht.waatea
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ client/waatea2_client/android.bak_2025-08-26_1845/app/src/main/kotlin/com/example/waatea2_client/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.waatea2_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
